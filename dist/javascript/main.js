0cd4e9436fac1ec830cb53511a367f9cf0d6305f
Ayesha <ayeshasolanki151@gmail.com>
2024-02-18 22:32:32 +0530

    Errors Corrected

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -3,48 +3,48 @@ const TypeWriter=function(txtElement,words,wait=3000){
   this.words= words;
   this.txt ='';
   this.wordIndex=0;
-this.wait=parseInt(wait,10);
-this.type();
-this.isDeleting=false;
+  this.wait=parseInt(wait,10);
+  this.type();
+  this.isDeleting=false;
 }
 //Type Method
 TypeWriter.prototype.type= function(){
   //Current index of word
-const current = this.wordIndex % this.words.length;
-//Get full text of current word
-const fulltxt = this.words[current];
+  const current = this.wordIndex % this.words.length;
+  //Get full text of current word
+  const fulltxt = this.words[current];
 
-//check if deleting
-if(this.isDeleting) {
-  //Remove char
-  this.txt=fulltxt.substring(0,this.txt.length - 1);
-}else{
-  //Add char
-  this.txt=fulltxt.substring(0,this.txt.length + 1);
-}
+  //check if deleting
+  if(this.isDeleting) {
+    //Remove char
+    this.txt=fulltxt.substring(0,this.txt.length - 1);
+  }else{
+    //Add char
+    this.txt=fulltxt.substring(0,this.txt.length + 1);
+  }
 
-//Insert txt into element
-this.txtElement.innerHTML =`<span class="txt">${this.txt}</span>`;
+  //Insert txt into element
+  this.txtElement.innerHTML =`<span class="txt">${this.txt}</span>`;
 
-//initial type speed
-let typeSpeed =300;
-if(this.isDeleting){
-  typeSpeed /=2;
-}
+  //initial type speed
+  let typeSpeed =300;
+  if(this.isDeleting){
+    typeSpeed /=2;
+  }
 
-//If word is complete
-if(!this.isDeleting && this.txt===fulltxt){
-  //Make a pause at end
-  typeSpeed= this.wait;
-  //Set delete to true
-  this.isDeleting =true;
+  //If word is complete
+  if(!this.isDeleting && this.txt===fulltxt){
+    //Make a pause at end
+    typeSpeed= this.wait;
+    //Set delete to true
+    this.isDeleting =true;
   }
   else if(this.isDeleting && this.txt ===''){
-this.isDeleting=false;
-//Move to next word
-this.wordIndex++;
-//Pause before startb typing
-typeSpeed=500;
+    this.isDeleting=false;
+    //Move to next word
+    this.wordIndex++;
+    //Pause before startb typing
+    typeSpeed=500;
   }
 
   setTimeout(() => this.type(), 500)
